57e70b454815b0c080aa63e6c58ea33ed8dc0a7b
tima <timka.0416@gmail.com>
2023-01-26 14:21:13 +0600

    Initial Commit

---
+++ hw1Month4/OrderTypeCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  OrderTypeCollectionViewCell.swift
+//  hw1Month4
+//
+//  Created by Mac on 27/1/2023.
+//
+
+import UIKit
+
+class OrderTypeCollectionViewCell: UICollectionViewCell {
+    
+    static let reuseIdentifierForOrderType = String(describing: TypeOfOrder.self)
+    @IBOutlet private weak var typeOfOrderLable: UILabel!
+    @IBOutlet private weak var deliveryImage: UIImageView!
+    func display(item: TypeOfOrder) {
+        typeOfOrderLable.text = item.orderOfType
+        typeOfOrderLable.layer.borderColor = UIColor.lightGray.cgColor
+        typeOfOrderLable.layer.cornerRadius = 20
+        typeOfOrderLable.layer.borderWidth = 2
+        typeOfOrderLable.textColor = item.colorForText
+    }
+}
+
+struct TypeOfOrder {
+    let orderOfType: String
+    let colorForText: UIColor
+    let backGroundColorForText: UIColor
+}
+
